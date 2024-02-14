c94d4d826a5d5d317c53f6a21d2a708552ecc1c9
Hp2003 <panchalhirenm123@gmail.com>
2024-02-14 13:02:03 +0530

    added new layout for chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,30 @@ export default {
                 '0%' : {width: "0%"},
                 '100%' : {width: "33.333333%"},
             },
+            newSlideIn : {
+                '0%' : { width: '300px' },
+                '100%' : { wdith: '0px' },
+            },
+            newSlideOut : {
+                '0%' : { width: '0px' },
+                '100%' : { wdith: '300px' },
+            },
+            closeOptions : {
+                '0%' : { width: '80px' },
+                '100%' : { wdith: '0px' },
+            },
+            openOptions : {
+                '0%' : { width: '0px' },
+                '100%' : { wdith: '80px' },
+            }
         },
         animation:{
             slideIn: 'slideIn 1s ease-in-out forwards',
             slideOut: 'slideOut 1s ease-in-out forwards',
+            newSlideIn: 'newSlideIn 1s ease-in-out forwards',
+            newSlideOut: 'newSlideOut 1s ease-in-out forwards',
+            closeOptions: 'closeOptions 1s ease-in-out forwards',
+            openOptions: 'openOptions 1s ease-in-out forwards',
         }
     },
   },
